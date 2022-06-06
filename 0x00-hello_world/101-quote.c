a958951e1bd3fa1a73edff1ee848593069e9b46f
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-06 16:19:52 -0700

    PrintingWithPutchar()

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,6 @@
 
 int main(void)
 {
-	char *s = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-        putchar (*s);
-	return (1);
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
 }
