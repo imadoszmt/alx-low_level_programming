5f18b9bbdc8104f6e23a97ed00b41fa733de37ac
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-06 12:30:00 -0700

    PrintingWithPutsFunction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- *Entry point - main
+ *main - Entry point
  *Program that prints "Programming is like building a multilingual puzzle"
  *Return 0 (Success)
  */
@@ -9,5 +9,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+	return (0);
 }
