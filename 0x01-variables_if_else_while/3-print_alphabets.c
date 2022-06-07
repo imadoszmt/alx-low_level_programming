ec070d475fe24138265fc06ba88fa5cb55533336
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-07 09:51:01 -0700

    LowerUpperCaseAlphabets

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - printing lowercase followed by uppercase alphabets
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a', ch <= 'Z', ch++)
+	{
+		putchar (ch);
+	}
+	putchar('\n');
+
+	return (0);
+}
