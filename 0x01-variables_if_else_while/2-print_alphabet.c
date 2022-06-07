8d5491db952ea847ecfd068e739118b7b32b1762
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-07 08:56:32 -0700

    LowercaseAlphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - printing alphabets in a lowercase.
+ *
+ * Return: 0 (Success)
+ */
+
+int main (void)
+{
+	char ch,
+	
+	for ch in [a..z]
+		putchar (ch);
+
+
+	return (0);
+}
