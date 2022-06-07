f5393370bc863ee6fe77955da20a5806d1515b21
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-07 12:00:39 -0700

    LowerCaseExcludinge

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - print lowercase alphabet except 'q' and 'e'.
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (char != 'e','q')
+	{
+		putchar (ch);
+	}
+	putchar('\n');
+	return (0);
+}
