6dc0c3f593b8e4cdd6620554a81678a2cef01a98
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-07 19:29:11 -0700

    Singledigits>,>space

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - printing single digits separated by "," and space " ".
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = '0'; num <= '9'; num++)
+	{
+		putchar(num);
+		putchar(",");
+		putchar(" ");
+	}
+	putchar("\n");
+	return (0);
+}
