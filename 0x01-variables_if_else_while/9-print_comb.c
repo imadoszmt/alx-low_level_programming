130949cd54238e7ba41906a64bc644027ca93637
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-08 07:35:03 -0700

    Singledigits>,>space

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,18 @@ int main(void)
 {
 	int num;
 
-	for (num = '0'; num <= '9'; num++)
+	for (num = '0'; num <= '9'; ++num)
+
 	{
 		putchar(num);
-		putchar(',');
-		putchar(' ');
+		
+		if (num != '9')
+		{
+			putchar(',');
+                	putchar(' ');
+		}
 	}
+
 	putchar('\n');
 	return (0);
 }
