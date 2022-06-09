bd53ab371b2967f1de3911941865d0abc30dcb07
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-08 18:29:28 -0700

    CombinationOfTwo#Nums

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - printing combinations of different '2' numbers, followed with ',' and "space".
+ *
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int num;
+	int num1;
+
+	for (num = '0'; num <= '9' ; num++)
+	{
+		for (num1 = '0'; num1 <= '9'; num1++)
+		{	
+			if (num != num1 && num < num1)
+		        {
+				putchar(num);
+				putchar(num1);
+		        	putchar(',');
+				putchar(' ' );
+			}
+
+		}	
+	}
+	putchar('\n');
+	return (0);
+
+}
