3d17e98b6a3426c2ba652dcfd6fa0e3e856f91a0
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-12 20:12:53 -0700

    Print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include <holbertonschool.h>
+
+/**
+ * main - a program that prints "_putchar".
+ *
+ *Return: 0 (Success)
+ */
+
+int main(void)
+{
+	char = "_putchar";
+
+	for ( c = 0, c != 'NULL', c++)
+	{
+		_putchar(c);
+	}
+	
+	putchar('\n');
+	return (0);
+}
