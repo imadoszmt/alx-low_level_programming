a047b13f1ea3bf6c6fb4338ebdba623766cbc37e
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-13 17:41:21 -0700

    PrintingLowercasesX10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10: printing lowercases "10 times", followed by "new ligne".
+ *
+ * Return: void
+ */
+
+void print_alphabet_x10(void)
+{
+	int times;
+	char ch;
+	for (times = 1; times <= 10; times++)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar (ch);
+		}
+		_putchar ('\n');
+	
+	}
+}
