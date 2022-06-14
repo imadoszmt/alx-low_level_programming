f67c277e78870ee3bdcede5179615eb1c9f47d8d
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-13 18:54:05 -0700

    CheckingLowercase

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * islower - checking if characters is lowercase.
+ *
+ * Return: 1 (if lowercase), 0 (otherwise)
+ */
+
+int _islower(int c)
+{
+	char ch, ch1;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+	{
+		if (c = ch)
+		{
+			return (1);
+		}
+		else:
+			return (0);
+	}
+}
