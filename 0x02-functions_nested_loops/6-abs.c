8af2ba2bd4471ad9aa9c950f49b6e8be4f9518cc
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-14 16:57:16 -0700

    AbsoluteValueOfInteger

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _abs - A function that print the absolute value of an integer.
+ * Parameter: Type integer.
+ * Return: 0 (Success).
+ */
+
+int _abs(int)
+{
+	if (n < 0)
+	{
+		int c;
+
+		c = n* (-1);
+		printf(c);
+	}
+	else
+	{
+		printf(n);
+	}
+	return (0);
+}
