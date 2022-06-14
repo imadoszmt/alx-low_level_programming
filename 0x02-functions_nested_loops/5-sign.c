55ec2e0574f8e6c415e35eebbcf1315a93db47aa
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-14 13:58:53 -0700

    CheckingSignOfIntegers

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -10,21 +10,21 @@ int print_sign(int n)
 {
 	if (n > 0)
 	{
-		return(1);
 		_putchar('+');
 		_putchar('1');
+		return(1);
 	}
 	else if (n < 0)
 	{
-		return(-1);
 		_putchar('-');
 		_putchar('1');
+		return(-1);
 	}
 	else
 	{
+		_putchar('0');
+		_putchar('0');
 		return (0);
-		_putchar('0');
-		_putchar('0');
 	}
 }
 
