9c6385d1c1ce30c39361718ed8f1386045796011
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-14 20:22:13 -0700

    24Hours

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * jack_bauer - A function that print every minute of the day.
+ *
+ * Return: Void
+ */
+
+void jack_bauer(void)
+{
+	int min0, min1, sec0, sec1;
+
+	for (min0 = 0; min0 <= 2; min0++)
+	{
+		printf("%d", min0);
+		for (min1 = 0; min1 <= 3; min1++)
+		{
+			printf("%d:", min1);
+
+			for (sec0 = 0; sec0 <= 5; sec0++)
+			{
+				printf("%d", sec0);
+
+				for (sec1 = 1; sec1 <= 9; sec1++)
+				{
+					printf("%d", sec1);
+				}
+
+			}
+
+		}
+
+	}
+return (0);
+}
+
