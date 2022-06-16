d42942dd2902bf79aae8513cfba0b5806a6ed18d
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-16 12:20:53 -0700

    9TimesTable

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,80 +1,39 @@
 #include "main.h"
 
 /**
- * time_table - A function that print times table of "9".
- *Parameter: void.
- *Return: void.
+ * times_table - A function that print the "9 times table".
+ * Parameter: Void
+ * Return: Void.
  */
 
 void times_table(void)
 {
-	int n = 0;
-	int n1, n2, n3, n4, n5, n6, n7, n8, n9;
+	int n, n1;
+	int N = n * n1;
 
-	if (n == 0)
+	for (n = 0; n <= 9; n++)
 	{
 		for (n1 = 0; n1 <= 9; n1++)
 		{
-			for (n2 = 0; n2 <= 18; n2 += 2)
+
+			if (n1 == 0)
 			{
-				for (n3 = 0; n3 <= 27; n3 += 3)
-				{
-					for (n4 = 0; n4 <= 36; n4 += 4)
-					{
-						for (n5 = 0; n5 <= 45; n5 += 5)
-						{
-							for (n6 = 0; n6 <= 54; n6 += 6)
-							{
-								for (n7 = 0; n7 <= 63; n7 += 7)
-								{
-									for (n8 = 0; n8 <= 72; n8 += 8)
-									{
-										for (n9 = 0; n9 <= 81; n9 += 9)
-										{
-											_putchar('0' + n);
-											_putchar(',');
-											_putchar(' ');
-											_putchar('0' + n1);
-											_putchar(',');
-											_putchar(' ');
-											_putchar('0' + n2);
-											_putchar(',');
-											_putchar(' ');
-											_putchar('0' + n3);
-											_putchar(',');
-											_putchar(' ');
-											_putchar('0' + n4);
-											_putchar(',');
-											_putchar(' ');
-											_putchar('0' + n5);
-											_putchar(',');
-											_putchar(' ');
-											_putchar('0' + n6);
-											_putchar(',');
-											_putchar(' ');
-											_putchar('0' + n7);
-											_putchar(',');
-											_putchar(' ');
-											_putchar('0' + n8);
-											_putchar(',');
-											_putchar(' ');
-											_putchar('0' + n9);
-										}
-
-									}
-
-								}
-
-							}
-
-						}
-
-					}
-
-				}
-
+				_putchar('0' + N);
 			}
-
+			else if (N <= 9)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + N);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar('0' + (N / 10));
+				_putchar('0' + (N % 10));
+			}
 		}
+		_putchar('\n');
 	}
 }
