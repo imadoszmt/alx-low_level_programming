3593979c0bab4db6fe36b8d341b12359cd11b18a
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-17 13:13:54 -0700

    TimesTable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,20 +7,17 @@
 void print_times_table(int n)
 {
 	int n1, n2, N;
-	
+
 	if (n >= 0 && n < 15)
 	{
 		for (n1 = 0; n1 <= n; n1++)
 		{
 			for (n2 = 0; n2 <= n; n2++)
 			{
-				N = n1 * n2;
-				if (n2 == 0)
+				N = n1 * n2;}
+				if (N < 10)
 				{
 					_putchar('0' + N);
-				}
-				else if (N < 10)
-				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
