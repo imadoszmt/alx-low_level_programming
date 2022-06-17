5a65b5e8d752e2b244bd9b3e36fc9831b67f59d7
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-17 13:01:33 -0700

    TimesTable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,41 +11,41 @@ void print_times_table(int n)
 
 	if (n >= 0 && n < 15)
 	{
-	for (n1 = 0; n1 <= n; n1++)
-	{
-		for (n2 = 0; n2 <= n; n2++)
+		for (n1 = 0; n1 <= n; n1++)
 		{
-			N = n1 * n2;
-			if (n2 == 0)
+			for (n2 = 0; n2 <= n; n2++)
 			{
-				_putchar('0' + N);
+				N = n1 * n2;
+				if (n2 == 0)
+				{
+					_putchar('0' + N);
+				}
+				else if (N < 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + N);
+				}
+				else if (N >= 10 && N <= 99)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + (N / 10));
+					_putchar('0' + (N % 10));
+				}
+				else
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar('0' + (N / 100));
+					_putchar('0' + ((N % 100) / 10));
+					_putchar('0' + (N % 10));
+				}
 			}
-			else if (N < 10)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + N);
-			}
-			else if (N >= 10 && N <= 99)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar('0' + (N / 10));
-				_putchar('0' + (N % 10));
-			}
-			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar('0' + (N / 100));
-				_putchar('0' + ((N % 100) / 10));
-				_putchar('0' + (N % 10));
-			}
+			_putchar('\n');
 		}
-		_putchar('\n');
-	}
 	}
 }
