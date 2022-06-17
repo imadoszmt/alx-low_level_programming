ac746cc998be87acbb5c7ae6caef14e3474b45b4
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-17 09:21:55 -0700

    TimesTable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -11,37 +11,36 @@ void print_times_table(int n)
 	int n1, n2;
 	int N;
 
-	if ((n != 0) || (n != 15))
+	for (n1 = 0; n1 <= n; n++)
 	{
-		for (n1 = 0; n1 <= n; n++)
+		for (n2 = 0; n2 <= n; n2++)
 		{
-			for (n2 = 0; n2 <= n; n2++)
+			N = n * n1;
+			if (n2 == 0)
 			{
-				N = n * n1;
-				if (n2 == 0)
-				{
-					_putchar('0' + N);
-				}
-				else if (N <= n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + N);
-				}
-				else
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (N / 10));
-					_putchar('0' + (N % 10));
-				}
+				_putchar('0' + N);
 			}
-		
+			else if (N <= n)
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + N);
+			}
+			else
+			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar('0' + (N / 10));
+				_putchar('0' + (N % 10));
+			}
 		}
+		
 	}
 	_putchar('\n');
-
 }
+
+
+
