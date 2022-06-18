67cefd883d95b071c242097c09c4a2390830d611
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-17 20:37:27 -0700

    SumOfMultiplesOf3&5

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -12,9 +12,9 @@ int main(void)
 	int sum;
 
 	sum = 0;
-	for (n = 0; n < 1024; n ++)
+	for (n = 0; n < 1024; n++)
 	{
-		if ((n % 3) || (n % 5))
+		if ((n % 3 == 0) || (n % 5 == 0))
 		{
 			sum = sum + n;
 		}
