2f085356026863f7b3f2bbb3625fe19ede6cfa30
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-18 12:57:09 -0700

    SumEvenValueOfFibonacciNumbers

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -13,15 +13,16 @@ int main(void)
 	unsigned long sum;
 
 	sum = 0;
-	for (f2 = 1,f1 = 0; sum <= 4000000; f2 = f2 + f1, f1 = f2 - f1)
+	for (f2 = 1, f1 = 0; sum <= 4000000; f2 = f2 + f1, f1 = f2 - f1)
 	{
 		if (f2 % 2 == 0)
 		{
 			sum = sum + f2;
-			printf("%ld", sum);
+
 		}
-		
+
 	}
+	printf("%ld", sum);
 	printf("\n");
 	return (0);
 }
