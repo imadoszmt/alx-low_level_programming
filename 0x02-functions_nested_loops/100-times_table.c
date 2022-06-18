ca5188f313ba67a6e57076c376abbd1662fb99c7
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-17 18:13:09 -0700

    FixTimesTable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -15,13 +15,15 @@ void print_times_table(int n)
 			for (n2 = 0; n2 <= n; n2++)
 			{
 				N = n1 * n2;
-				if (n2 == 0 || N < 10)
-				{
+				if (n2 == 0)
 					_putchar('0' + N);
+				else if (N < 10)
+				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
+					_putchar('0' + N);
 				}
 				else if (N >= 10 && N <= 99)
 				{
