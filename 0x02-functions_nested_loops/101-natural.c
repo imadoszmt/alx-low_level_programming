e26ade607703b3942894f8f92d854cfab2bf2b54
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-17 19:13:12 -0700

    SumOfMultiplesOf3

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Print all multiples of "3" below 1024 (excluded).
+ *
+ * Return: Integer.
+ */
+
+int main(void)
+{
+	int n;
+	int sum;
+
+	for (n = 3; n < 1024; n += 3)
+	{
+		sum = n
+	}
+	printf("%d\n", sum);
+}
