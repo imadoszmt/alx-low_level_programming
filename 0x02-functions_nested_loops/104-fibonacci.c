8c5d69febe1ebde3256100ea79216b9a8d9a5d7c
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-18 20:54:26 -0700

    First98FibonacciNumbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -8,24 +8,38 @@
 
 int main(void)
 {
-	unsigned long long  f1 = 0;
-	unsigned long long  f2 = 1;
+	unsigned long f1 = 0;
+	unsigned long f2 = 1;
 	int n;
+	unsigned long f1_part1, f1_part2, f2_part1, f2_part2, f12_part1, f12_part2;
 	
 
 	for (n = 0; n <= 98; n++)
 	{
-		if (n <= 97)
+		if (n <= 92)
 		{
 			f2 = f2 + f1;
 			f1 = f2 - f1;
-			printf("%llu, ", f2);
+			printf("%lu, ", f2);
 		}
-		else if (n == 98)
+
+		f1_part1 = f1 / 1000000000;
+		f1_part2 = f1 % 1000000000;
+		f2_part1 = f2 / 1000000000;
+		f2_part2 = f2 % 1000000000;
+
+		else if (n >= 93)
 		{
-			f2 = f2 + f1;
-			f1 = f2 - f1;
-			printf("%llu", f2);
+			f12_part1 = f1_part1 + f2_part1;
+			f12_part2 = f1_part2 + f2_part2;
+
+			printf(", %lu%lu", f12_part1, f12_part2);
+			
+			f1_part1 = f2_part1;
+			f1_part2 = f2_part2;
+			f2_part1 = f12_part1;
+			f2_part2 = f12_part2;
+
 		}
 	}
 	printf("\n");
