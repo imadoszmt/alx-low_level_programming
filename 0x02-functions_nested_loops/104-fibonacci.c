0436459c8f26dee09bea7b1fdb0c88c9d097725d
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-19 09:43:13 -0700

    First98FibonacciNumbers

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,15 +14,15 @@ int main(void)
 	unsigned long f1_part1, f1_part2, f2_part1, f2_part2, f12_part1, f12_part2;
 	
 
-	for (n = 0; n <= 98; n++)
+	for (n = 0; n <= 97; n++)
 	{
-		if (n <= 92)
+		if (n <= 91)
 		{
 			f2 = f2 + f1;
 			f1 = f2 - f1;
 			printf("%lu, ", f2);
 		}			
-		else if (n >= 93)
+		else if (n >= 92)
 		{
 			f1_part1 = f1 / 1000000000;
 			f1_part2 = f1 % 1000000000;
