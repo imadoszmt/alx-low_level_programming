bceb40ca1a64bd480c456ad9e619f0dd83cf0725
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-19 19:10:15 -0700

    LateNightFixFibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -14,33 +14,33 @@ int main(void)
 	unsigned long f1_part1, f1_part2, f2_part1, f2_part2, f12_part1, f12_part2;
 	
 
-	for (n = 0; n <= 97; n++)
+	for (n = 0; n <= 91; n++)
 	{
-		if (n <= 91)
-		{
-			f2 = f2 + f1;
-			f1 = f2 - f1;
-			printf("%lu, ", f2);
-		}			
-		else if (n >= 92)
-		{
-			f1_part1 = f1 / 1000000000;
-			f1_part2 = f1 % 1000000000;
-			f2_part1 = f2 / 1000000000;
-			f2_part2 = f2 % 1000000000;
-			excess = (f1_part2 + f2_part2) / 1000000000;
-			f12_part2 = (f1_part2 + f2_part2) - (excess * 1000000000);
-			f12_part1 = (f1_part1 + f2_part1) + excess;
-
-			printf(", %lu%lu", f12_part1, f12_part2);
-			
-			f1_part1 = f2_part1;
-			f1_part2 = f2_part2;
-			f2_part1 = f12_part1;
-			f2_part2 = f12_part2;
-
-		}
+		f2 = f2 + f1;
+		f1 = f2 - f1;
+		printf("%lu, ", f2);
 	}
+					
+		f1_part1 = f1 / 1000000000;
+		f1_part2 = f1 % 1000000000;
+		f2_part1 = f2 / 1000000000;
+		f2_part2 = f2 % 1000000000;
+
+	for (n = 92; n <= 97; n++)
+	{
+		excess = (f1_part2 + f2_part2) / 1000000000;
+		f12_part2 = (f1_part2 + f2_part2) - (excess * 1000000000);
+		f12_part1 = (f1_part1 + f2_part1) + excess;
+
+		printf(", %lu%lu", f12_part1, f12_part2);
+			
+		f1_part1 = f2_part1;
+		f1_part2 = f2_part2;
+		f2_part1 = f12_part1;
+		f2_part2 = f12_part2;
+
+	}
+
 	printf("\n");
 	return (0);
 }
