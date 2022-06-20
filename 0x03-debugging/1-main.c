5392285dea696842481d28b9372ce44677063461
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-19 17:06:03 -0700

    InfiniteLoop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -13,11 +13,11 @@ int main(void)
 
 	i = 0;
 
-	while (i < 10) /* while condition will always be true since there is no i++ */
-	{
-		putchar(i);
-	}
-
+	/* while (i < 10)
+	*{
+	*	putchar(i);
+	*}
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
