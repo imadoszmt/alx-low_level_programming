8a64b83cf7b9ea797861edc213232c4bce4030e4
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-20 12:36:02 -0700

    FixPrintReaminingDays

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -10,9 +10,9 @@
 * Return: void
 */
 
-void print_remaining_days(int month, int day, int year)
+void print_remaining_days(int month, int day, int yeari)
 {
-	if ((year % 100 == 0 || year % 400 == 0) && (year % 4 == 0))
+	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
 	{
 		if (month > 2 && day >= 60)
 		{
