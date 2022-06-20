31ebf8f692174f297f08510b4deb40cc5e829fed
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-19 19:20:26 -0700

    LateNightFixFibonacci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -12,7 +12,7 @@ int main(void)
 	unsigned long f2 = 1;
 	int n, excess;
 	unsigned long f1_part1, f1_part2, f2_part1, f2_part2, f12_part1, f12_part2;
-	
+
 
 	for (n = 0; n <= 91; n++)
 	{
@@ -20,7 +20,7 @@ int main(void)
 		f1 = f2 - f1;
 		printf("%lu, ", f2);
 	}
-					
+
 		f1_part1 = f1 / 1000000000;
 		f1_part2 = f1 % 1000000000;
 		f2_part1 = f2 / 1000000000;
@@ -33,13 +33,13 @@ int main(void)
 		f12_part1 = (f1_part1 + f2_part1) + excess;
 
 		printf("%lu%lu, ", f12_part1, f12_part2);
-			
+
 		f1_part1 = f2_part1;
 		f1_part2 = f2_part2;
 		f2_part1 = f12_part1;
 		f2_part2 = f12_part2;
 
-		if (n = 97)
+		if (n == 97)
 		{
 			printf("%lu%lu", f12_part1, f12_part2);
 		}
