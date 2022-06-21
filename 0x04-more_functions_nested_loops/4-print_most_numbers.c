2490d076d5e8d60fe4707de6817f7a32e0072fbc
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-21 07:50:01 -0700

    PrintFrom0-To-9Except2-&-4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Prints numbers from 0 to 9,except 2 and 4.
+ *
+ * Return: Void.
+ */
+
+void print_most_numbers(void)
+{
+	char c;
+
+	while (c <= 9)
+	{
+		if (c != 2 || c != 4)
+		{
+			_putchar('0' + c);
+		}
+	}
+	_putchar('\n');
+}
