049c2382f0b6102f0dbc8643d16178bef0dce3e3
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-21 18:55:59 -0700

    MoreNumbersFix

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -6,3 +9,4 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
+#endif
