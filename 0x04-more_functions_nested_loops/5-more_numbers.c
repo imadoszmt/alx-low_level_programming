e481b5948acd690846abd7618bebf5a539968528
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-24 08:08:11 -0700

    MoreNumbersFix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -20,14 +20,13 @@ void more_numbers(void)
 			if (num <= 9)
 			{
 				_putchar('0' + num);
-				num++;
 			}
 			else if (num > 9)
 			{
-				_putchar('0' + num / 10);
-				_putchar('0' + num % 10);
-				num++;
+				_putchar('0' + (num / 10));
+				_putchar('0' + (num % 10));
 			}
+			num++;
 		}
 		_putchar('\n');
 		lines++;
