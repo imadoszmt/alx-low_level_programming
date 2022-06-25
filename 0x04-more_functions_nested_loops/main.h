537b2849af7325fca7a18823805036946d413ae2
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-24 20:26:28 -0700

    FixMainH

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,3 +1,6 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
@@ -7,3 +10,5 @@ void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
 void print_diagonal(int n);
+
+#endif /* MAIN_H */
