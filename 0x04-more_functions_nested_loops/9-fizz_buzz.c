2a811dc9903ee65b69fe102bf5241847843a999b
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-25 12:01:10 -0700

    FizzBuzzInsertion

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * main - Print "Fizz" for multiples of '3', "Buzz" for multiples of '5',
+ * "FizzBuzz" for both, starting from 0 to 100.
+ *
+ * Return: 0 (Succes).
+ */
+
+int main(void)
+{
+	int n = 1;
+	char fizz[] = "Fizz";
+	char buzz[] = "Buzz";
+
+	while (n <= 100)
+	{
+		if (n % 3 == 0)
+		{
+			printf("%s", fizz);
+		}
+	       		if (n % 5 == 0)
+			{
+				printf("%s", buzz);
+			}
+		else if ((n % 3 != 0) && (n % 5!= 0))
+		{
+			printf("%d", n);
+		}
+		printf(" ");
+		n++;
+	}
+	printf("\n");
+
+}
