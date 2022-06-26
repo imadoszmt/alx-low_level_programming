ba3c3d341c105d7d8f40cfa2b799018e63f9aad4
Imad Zalmat <imaden.zalmat@gmail.com>
2022-06-25 20:19:24 -0700

    PrintTriangleFix

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -13,25 +13,20 @@ void print_triangle(int size)
 	if (size > 0)
 	{
 		nline = 0;
-
 		while (nline < size)
 		{
 			nspace = 1;
-
 			while (nspace < size - nline)
 			{
 				_putchar(' ');
 				nspace++;
 			}
-
 			nhash = 0;
-
 			while (nhash <= nline)
 			{
 				_putchar('#');
 				nhash++;
 			}
-
 		_putchar('\n');
 		nline++;
 		}
