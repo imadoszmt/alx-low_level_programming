800c2305a4e7d5ac5de53e97b3534fa7cc778991
Imad Zalmat <imaden.zalmat@gmail.com>
2022-07-05 10:13:06 -0700

    ReverseString

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_rev - prints a string in reverse.
+ * @s: pointer to character.
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	for (i = _strlen(s) - 1; s[i] != '\0'; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
