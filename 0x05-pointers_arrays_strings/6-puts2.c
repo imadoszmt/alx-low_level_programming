bc2a0cb8bc853094f6596043ee061878b7450f6d
Imad Zalmat <imaden.zalmat@gmail.com>
2022-07-08 16:17:36 -0700

    PrintOtherCharacters

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,9 +10,9 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	for (i = 0; str[i] > 0; i += 2)
 	{
-		_putchar(str[i] + '0');
+		_putchar(str[i]);
 	}
 	_putchar('\n');
 }
