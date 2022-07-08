3ac0732120317f425344488cc301813de5b6d847
Imad Zalmat <imaden.zalmat@gmail.com>
2022-07-08 11:05:58 -0700

    StylisticReverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * rev_string - a function that reverses a string.
+ * @s: a pointer to character.
+ *
+ */
+
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+
+	j = 0;
+	for (i = _strlen(s) - 1; s[i] != '\0'; i--)
+	{
+		temp = s[j];
+		s[j] = s[i];
+		s[i] = temp;
+	j++;	
+	}
+	
+}
+
+/**
+ * _strlen - return the lenght of a string.
+ * @s: pointer to a character.
+ *
+ * Return: lenght (integer).
+ */
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0';)
+		count++;
+	return (count);
+}
