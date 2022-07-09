8039f50fbb8dd66c6473c086c81db42f3ac82ba5
Imad Zalmat <imaden.zalmat@gmail.com>
2022-07-08 18:11:25 -0700

    Print2ndHalfOfString

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,17 +9,17 @@
 void puts_half(char *str)
 {
 	int i;
-	
+
 	if (_strlen(str) % 2 == 0)
 	{
-		for (i = _strlen(str) / 2 ; i <= _strlen(str); i++)
+		for (i = _strlen(str) / 2 ; i < _strlen(str); i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else if (_strlen(str) % 2 != 0)
 	{
-		for (i = (_strlen(str) - 1) / 2; i <= _strlen(str); i++)
+		for (i = (_strlen(str) - 1) / 2; i < _strlen(str); i++)
 		{
 			_putchar(str[i]);
 		}
