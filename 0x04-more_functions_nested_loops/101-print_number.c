d1d5707040d8438dbc47c5bcb8f8a64ab6176ec9
Imad Zalmat <imaden.zalmat@gmail.com>
2022-07-21 09:05:06 -0700

    PrintIntegerFix

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,31 +8,41 @@
 
 void print_number(int n)
 {
-	if (n <= 9)
-		_putchar('0' + n);
-	else if (n >= 10 && n <= 99)
+	if (n < 0)
 	{
-		if (n < 0)
-		{
-			n = -n
-			_putchar('-');
-		}
-		else
+		n = -n;
+
+		_putchar('-');
+	}
+	else
+		n = n;
+	while (n / 10)
+	{
+		if (n / 10 <= 9)
 		{
 			_putchar('0' + (n / 10));
 			_putchar('0' + (n % 10));
+			break;
+		}
+		else if (n / 100 <= 9)
+		{
+			_putchar('0' + (n / 100));
+			_putchar('0' + (n % 100) / 10);
+			_putchar('0' + (n % 10));
+			break;
+		}
+		else if (n / 1000 <= 9)
+		{
+			_putchar('0' + (n / 1000));
+			_putchar('0' + (n % 1000) / 100);
+			_putchar('0' + (n % 100) / 10);
+			_putchar('0' + (n % 10));
+			break;
+		}
+		else if (n == 0)
+		{	
+			_putchar('0' + n);
+			break;
 		}
 	}
-	else if (n >= 100 && n <= 999)
-	{
-		_putchar('0' + (n / 100));
-		_putchar('0' + (n / 10) % 10);
-		_putchar('0' + (n % 10));
-	}
-	else if (n >= 1000 && n <= 9999)
-	{
-		_putchar('0' + (n / 1000));
-		_putchar('0' + (n / 100) % 10);
-		_putchar('0' + (n % 100) / 10);
-		_putchar('0' + (n / 10));
-	}
+}
