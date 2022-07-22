a5d3894a901c17c50998260d5b6c6ae39f02cce5
Imad Zalmat <imaden.zalmat@gmail.com>
2022-07-22 10:53:38 -0700

    FixPrintNumber

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,41 +8,22 @@
 
 void print_number(int n)
 {
+	unsigned m, d = 1;
+
 	if (n < 0)
 	{
 		n = -n;
-
 		_putchar('-');
 	}
 	else
 		n = n;
-	while (n / 10)
+
+	for (m = n; m > 9; m = m / 10)
 	{
-		if (n / 10 <= 9)
-		{
-			_putchar('0' + (n / 10));
-			_putchar('0' + (n % 10));
-			break;
-		}
-		else if (n / 100 <= 9)
-		{
-			_putchar('0' + (n / 100));
-			_putchar('0' + (n % 100) / 10);
-			_putchar('0' + (n % 10));
-			break;
-		}
-		else if (n / 1000 <= 9)
-		{
-			_putchar('0' + (n / 1000));
-			_putchar('0' + (n % 1000) / 100);
-			_putchar('0' + (n % 100) / 10);
-			_putchar('0' + (n % 10));
-			break;
-		}
-		else if (n == 0)
-		{	
-			_putchar('0' + n);
-			break;
-		}
+		d = d * 10;
+	}
+	for (d; d >= 1; d = d / 10)
+	{
+		_putchar('0' + (n / d) % 10);
 	}
 }
