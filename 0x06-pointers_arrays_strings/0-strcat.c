b04e95924cc95ee3536f31b459de9994fae819b5
Imad Zalmat <imaden.zalmat@gmail.com>
2022-07-31 10:42:43 -0700

    StringConcatenation

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strcat - A function that concatenate two strings.
+ * @dest: pointer to character.
+ * @src: pointer to character.
+ *
+ * Return: pointer to character.
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	while (*dest)
+	{
+		_putchar(*dest);
+		dest++;
+		break;
+		_putchar(*src);
+		*src++;
+	}
+	return(*dest);
+}
---
+++ 0x06-pointers_arrays_strings/README.md
@@ -0,0 +1,4 @@
+
+
+      More Projects intended for learning pointers, arrays, and strings in C programming language as part 
+		Alx-Holberton School Software engineering program.
