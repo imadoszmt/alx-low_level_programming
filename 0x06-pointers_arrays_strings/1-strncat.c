0eaec227cb37feacf680c49dbac46bea612a81c9
Imad Zalmat <imaden.zalmat@gmail.com>
2022-08-06 10:40:29 -0700

    AppendNBytesOfSource

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,54 @@
+#include "main.h"
+
+/**
+ * _strncat - a function that concatenates two strings.
+ * @dest: a pointer to character (the destination).
+ * @src: a pointer to character (the source).
+ * @n: interger (number of "src" bytes)
+ *
+ * Return: a pointer to character.
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	while (src[n])
+	{	
+		_strcpy(&dest[_strlen(dest)], src);
+	}
+	dest[_strlen(dest) + 1] = '\0';
+	return (dest);
+}
+/**
+ * _strlen - return the lenght of a string.
+ * @s: pointer to a character.
+ *
+ * Return: lenght (integer).
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0';)
+		count++;
+	return (count);
+}
+/**
+ * _strcpy - a function that copies string from "src" to "dest".
+ * @dest: pointer to characters
+ * @src: pointer to chracters.
+ *
+ * Return: character.
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < _strlen(src) + 1; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
