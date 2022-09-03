5598ecd47caeda1f51e5cf1aa3f43370685c877c
Imad Zalmat <imaden.zalmat@gmail.com>
2022-09-03 10:42:22 -0700

    ChangesStringToUpper

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,5 +1,4 @@
-#ifndef MAIN_H
-#define MAIN_H
+
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
@@ -7,5 +6,4 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
-char *string_toupper(char *);
-#endif /* MAIN_H */
+char *string_toupper(char *a);
