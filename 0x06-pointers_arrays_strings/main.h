a3ef44d96badba51eb3f533f812c2bb9b50b2847
Imad Zalmat <imaden.zalmat@gmail.com>
2022-09-16 09:38:18 -0700

    FixStringToUpper

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,5 @@
-
+#ifndef MAIN_H
+#define MAIN_H
 char *_strcat(char *dest, char *src);
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
@@ -7,3 +8,4 @@ char *_strncpy(char *dest, char *src, int n);
 int _strcmp(char *s1, char *s2);
 void reverse_array(int *a, int n);
 char *string_toupper(char *a);
+#endif /* MAIN_H */
