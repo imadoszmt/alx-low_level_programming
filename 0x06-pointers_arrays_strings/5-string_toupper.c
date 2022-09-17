bed10f05cdd26648e7c499f4d96428f46710a21e
Imad Zalmat <imaden.zalmat@gmail.com>
2022-09-17 13:46:36 -0700

    StringToUpperFix

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -20,39 +20,5 @@ char *string_toupper(char *a)
 		}
 		a++;
 	}
-	return(_strcpy (b, a));
-}
-/**
- * _strcpy - a function that copies string from "src" to "dest".
- * @dest: pointer to characters
- * @src: pointer to chracters.
- *
- * Return: character.
- */
-
-char *_strcpy(char *dest, char *src)
-{
-	int i;
-
-	for (i = 0; i < _strlen(src) + 1; i++)
-	{
-		dest[i] = src[i];
-	}
-
-	return (dest);
-}
-/**
- * _strlen - return the lenght of a string.
- * @s: pointer to a character.
- *
- * Return: lenght (integer).
- */
-
-int _strlen(char *s)
-{
-	int count;
-
-	for (count = 0; s[count] != '\0';)
-		count++;
-	return (count);
+	return(a);
 }
