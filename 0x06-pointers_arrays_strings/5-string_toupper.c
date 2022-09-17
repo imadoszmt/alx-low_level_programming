3d7127e9bc5de0b0bc25b1b33ae5cd7bbf6ea843
Imad Zalmat <imaden.zalmat@gmail.com>
2022-09-17 11:26:31 -0700

    FixStringToUpper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -11,6 +11,7 @@ char *string_toupper(char *a)
 {
 	char *b;
 
+	b = '\0';
 	while (*a)
 	{
 		if (*a >= 'a' && *a <= 'z')
@@ -19,7 +20,39 @@ char *string_toupper(char *a)
 		}
 		a++;
 	}
-	b = *(&a);
-	return (b);
+	return(_strcpy (b, a));
 }
+/**
+ * _strcpy - a function that copies string from "src" to "dest".
+ * @dest: pointer to characters
+ * @src: pointer to chracters.
+ *
+ * Return: character.
+ */
 
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i < _strlen(src) + 1; i++)
+	{
+		dest[i] = src[i];
+	}
+
+	return (dest);
+}
+/**
+ * _strlen - return the lenght of a string.
+ * @s: pointer to a character.
+ *
+ * Return: lenght (integer).
+ */
+
+int _strlen(char *s)
+{
+	int count;
+
+	for (count = 0; s[count] != '\0';)
+		count++;
+	return (count);
+}
