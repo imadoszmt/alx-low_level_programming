281d76c3ab78696d1dee09b7be3187158ed14dad
Imad Zalmat <imaden.zalmat@gmail.com>
2022-10-01 09:40:15 -0700

    Rot13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,41 +1,24 @@
 /**
  * rot13 - A function that encodes a string into "rot13"
  * (rotating letter by 13 places)
- * a@: a pointer to a string.
+ * @a: a pointer to a string.
  * Return: a pointer to a string.
  */
 
 char *rot13(char *a)
 {
-	char characters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm'};
-	char substitute[] = {'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
-	char upper_characters[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M'};
-	char upper_substitute[] = {'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
-	int i, j;
+	int i;
 
 	i = 0;
 	while (a[i])
 	{
-		j = 0;
-		while (characters[j] || substitute[j] || upper_characters[j] || upper_substitute[j])
+		if ((a[i] >= 'a' && a[i] <= 'm') || (a[i] >= 'A' && a[i] <= 'M'))
 		{
-			if (a[i] == characters[j])
-			{
-				a[i] = substitute[j];
-			}
-			else if (a[i] == substitute[j])
-			{
-				a[i] = characters[j];
-			}
-			else if (a[i] == upper_characters[j])
-			{
-				a[i] = upper_substitute[j];
-			}
-			else if (a[i] == upper_substitute[j])
-			{
-				a[i] = upper_characters[j];
-			}
-		j++;
+			a[i] = a[i] + 32;
+		}
+		else if ((a[i] >= 'n' && a[i] <= 'z') || (a[i] >= 'N' && a[i] <= 'Z'))
+		{
+			a[i] = a[i] - 32;
 		}
 	i++;
 	}
