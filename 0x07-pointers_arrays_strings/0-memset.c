c8d1af9623fcf146ea91ab3213375dbe0cfad1fa
Imad Zalmat <imaden.zalmat@gmail.com>
2023-08-05 11:09:10 -0700

    ImplementingMemsetFunction"

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _memset - a function that fills a block of memory with
+ * a particular value.
+ * @s: a pointer to the block of memory to be filled.
+ * @b: value that fills the block of memory.
+ * @n: number of bytes to be filled.
+ */
+
+char *_memset(char *s, char b, unsigned n)
+{
+	unsigned char c = (unsigned char) b;
+	for (int i = 0; i < n; i++)
+	{
+		*(s + i) = c;
+	}
+	return (s);
+}
