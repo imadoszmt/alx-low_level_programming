db3d0907b475f8d95cc4bef1ba44dbb6529d4bb7
Imad Zalmat <imaden.zalmat@gmail.com>
2023-08-05 11:48:18 -0700

    ImplementingMemsetFunction

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -8,10 +8,10 @@
  * @n: number of bytes to be filled.
  */
 
-char *_memset(char *s, char b, unsigned n)
+char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned char c = (unsigned char) b;
-	unsigned i;
+	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
