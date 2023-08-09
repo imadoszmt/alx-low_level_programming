df8c3ac051d1ba988a673100d51732b26bd2be4c
Imad Zalmat <imaden.zalmat@gmail.com>
2023-08-09 12:45:59 -0700

    FixStrchrFunction

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,39 +7,14 @@
  * @c: the searched character.
  *
  * Return: if 'c' is found return a pointer to the first occurrence
- * of the character, else return a null pointer.
+ * of the character.
  */
 
 char *_strchr(char *s, char c)
 {
-	int comparaison;
-
-	comparaison = _strcmp(s, &c);
-	while (comparaison != 0)
+	while (*s != c)
 	{
 		s++;
-		break;
 	}
 	return (s);
-}
-
-/**
- * _strcmp - a function that compares two strings "s1" and "s2".
- * @s1: a pointer to character.
- * @s2: a pointer to character.
- *
- * Return: integer
- */
-
-int _strcmp(char *s1, char *s2)
-{
-	int result;
-
-	for (; ((*s1 != '\0' && *s2 != '\0') && (*s1 == *s2)); )
-	{
-		s1++;
-		s2++;
-	}
-	result = *s1 - *s2;
-	return (result);
 }
