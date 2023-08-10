eb7cda3a5943383a26162241ba895fa734bab3c5
Imad Zalmat <imaden.zalmat@gmail.com>
2023-08-10 15:47:49 -0700

    MoreFixStrchrFunction

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,5 +1,3 @@
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
-int _strcmp(char *s1, char *s2);
-int _strlen(char *s);
