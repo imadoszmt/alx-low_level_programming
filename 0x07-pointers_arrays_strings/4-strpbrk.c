123db960cbc654d895deac06cb33de13a85e68da
Imad Zalmat <imaden.zalmat@gmail.com>
2023-08-15 13:57:52 -0700

    First implementation of string pointer break function

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * _strpbrk - A function that searches the first occurence of any
+ * character from "accept" string within 's' string.
+ * @s: A pointer to the null-terminating string we want to search.
+ * @accept: A pointer to the null-terminating string containing the 
+ * set of character we want to search for.
+ *
+ * Return: if found return the first occurrence of any character from
+ * "accept" string in 's' string.esle, return NULL.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	char *accept_begin = accept;
+
+	for (; *s != '\0'; s++)
+	{
+		for (; *accept != '\0'; accept++)
+		{
+			if (*s == *accept)
+			{
+				return (s);
+			}
+		}
+		if (*accept == '\0')
+		{
+			accept = accept_begin;
+		}
+	}
+Return (NULL);	
+}	
