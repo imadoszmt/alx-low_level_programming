5aab56640b689e539241bd2a6eb4246a220d30ff
Imad Zalmat <imaden.zalmat@gmail.com>
2023-10-08 08:53:51 -0700

    Fix local variable problem and using recursion instead

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -6,4 +6,6 @@ int _strlen(char *s);
 char *_strncpy(char *dest, char *src, int n);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
+int _strlen_recursion(char *s);
+
 #endif /* MAIN_H */
