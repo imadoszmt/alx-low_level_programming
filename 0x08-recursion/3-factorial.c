74b8009da7ef467ef9135b3466eef41a8ba9554f
Imad Zalmat <imaden.zalmat@gmail.com>
2023-10-08 09:17:47 -0700

    add factorial of an integer program

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - A function that returns the factorial of
+ * a given number.
+ * @n: factorial of number to be calculated.
+ *
+ * Return: return the factorial of "n" if n > 0, else "-1" if n < 0.
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}	
+	return (n* factorial (n - 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -7,5 +7,6 @@ char *_strncpy(char *dest, char *src, int n);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
 
 #endif /* MAIN_H */
