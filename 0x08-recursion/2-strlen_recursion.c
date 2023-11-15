1201b2dc87a8b01804591e5986e35c4a2cf47e1d
Imad Zalmat <imaden.zalmat@gmail.com>
2023-11-15 10:33:25 -0800

    Add string lenght function

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - A function that print the lenght of a string
+ * using recursion.
+ * @s: A pointer to the string counted.
+ *
+ * Return: An integer (number of characters)
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		return (1 + _strlen_recursion(s + 1));
+	}
+	else
+		return (0);
+
+}
