f8c31e7a6ca2eee9e4b48721feed89881927f410
Imad Zalmat <imaden.zalmat@gmail.com>
2023-11-15 09:31:03 -0800

    Add puts_recursion function

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - A function that prints a string followed by a
+ * new line using recursing.
+ * @s: A pointer to the string to be printed.
+ *
+ * Return: void.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		_putchar (*s);
+		_puts_recursion(s + 1);
+	}
+	else
+		_putchar ('\n');
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+"Welcome to the 'Learning Recursion in C' repository! This project is part of the Alx-Holberton School Software Engineering program and focuses on delving deep into the concept of recursion in the C programming language."
