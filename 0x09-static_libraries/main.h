2cf2932bc5fd80d1defe5d99de808b6abde9bd46
Imad Zalmat <imaden.zalmat@gmail.com>
2024-01-19 10:10:23 -0800

    modify header files structure to include functions prototypes and macros

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,9 @@
 #ifndef MAIN_H
 #define MAIN_H
+/**
+ * Defining functions prototypes
+ */
+
 int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
@@ -20,11 +24,9 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
-#endif
 
 /**
- * Defining Macros.
+ * Defining Macros for "NULL".
  */
-#ifndef NULL
 #define NULL ((void *)0)
 #endif
