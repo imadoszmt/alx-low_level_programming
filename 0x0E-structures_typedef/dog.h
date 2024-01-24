9420e4fb54d3a67dcb1ef266332e7da83bbd1947
Imad Zalmat <imaden.zalmat@gmail.com>
2024-01-23 17:57:30 -0800

    Fix some nuance

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,3 @@
-#ifndef DOG_H
-#define DOG_H
-
 /**
  * struct dog - a new data type structure with the multiple atributtes
  * @name: first memeber of the struct.
@@ -16,6 +13,4 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
-
-#endif
+};
