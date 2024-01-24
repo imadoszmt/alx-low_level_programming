7d95a1a8308be46e37f989e366660f97ef624e52
Imad Zalmat <imaden.zalmat@gmail.com>
2024-01-23 17:53:23 -0800

    Fix description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -2,12 +2,13 @@
 #define DOG_H
 
 /**
- * struct dog - a new data type structure with the atributtes "name,
- * age, owner"
+ * struct dog - a new data type structure with the multiple atributtes
  * @name: first memeber of the struct.
  * @age: second member of the struct.
  * @owner: third member of the struct.
  *
+ * Description: creating new data type "structure" with multiple
+ * variable of different type.
  */
 
 struct dog
