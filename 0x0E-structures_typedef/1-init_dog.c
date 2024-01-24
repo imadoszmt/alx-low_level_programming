b65548cb09ca0e840abb586876c8ea30c5fff0bc
Imad Zalmat <imaden.zalmat@gmail.com>
2024-01-23 19:01:52 -0800

    Check if pointer is defferent to NULL

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -7,11 +7,15 @@
  * @name: a pointer to a char.
  * @age: variable of type "float".
  * @owner: a pointer to a char.
+ *
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if ( d != '\0')
+	{	
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
