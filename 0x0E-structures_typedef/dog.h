d954282da7813d895315c163ca9c789180495d65
Imad Zalmat <imaden.zalmat@gmail.com>
2024-01-23 17:00:19 -0800

    Fix betty documentation for structure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,20 @@
-struct dog{
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a new data type structure with the atributtes "name,
+ * age, owner"
+ * @name: first memeber of the struct.
+ * @age: second member of the struct.
+ * @owner: third member of the struct.
+ *
+ */
+
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
-};
+}
+
+#endif
