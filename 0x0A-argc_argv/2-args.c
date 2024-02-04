54518be20aa602de1c9cf236e41bac9c532d2d4d
Imad Zalmat <imaden.zalmat@gmail.com>
2024-02-04 09:48:01 -0800

    Add function that print the arguments passed into prompt

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - A function that print all arguments it receives on the prompt.
+ * @argc: the arguments counter.
+ * @argv: an array of string that hold the provided arguments.
+ *
+ * Return: Exit status, an integer.
+ */
+
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
