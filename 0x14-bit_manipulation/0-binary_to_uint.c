d41f0118284b7bab7354dc1c97fc16af255a45e5
Imad Zalmat <imaden.zalmat@gmail.com>
2024-02-11 12:00:58 -0800

    Fix NULL condition and header file comment

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * binary_to_uint - A function that converts a binary number to an integer.
@@ -14,6 +15,12 @@ unsigned int binary_to_uint(const char *b)
 
 	sum = 0;
 	i = 0;
+
+	if (b == NULL)
+	{
+		return (0);
+	}
+
 	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,7 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/** header file that contains prototypes of functions that have been used in these tasks */
+/** prototypes of functions that have been used in these tasks */
 int _putchar(char c);
 unsigned int binary_to_uint(const char *b);
 
